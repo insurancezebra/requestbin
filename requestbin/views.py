205699c73f2abe935b7c9dda32aef6f64733cc96
Meetesh Karia <meetesh.karia@alumni.duke.edu>
2018-05-02 14:52:39 -0500

    Moved inspect functionality to new path and supported proxying of requests. Example 'curl -X GET http://localhost:4000/api.ipify.org-1h42xqk1/?format=json' will proxy to http://api.ipify.org/?format=json and store the result in bucket 1h42xqk1

--- requestbin/views.py
+++ requestbin/views.py
@@ -1,5 +1,6 @@
+import requests
 import urllib
-from flask import session, redirect, url_for, escape, request, render_template, make_response
+from flask import session, redirect, url_for, escape, request, render_template, make_response, Response, stream_with_context
 
 from requestbin import app, db
 
@@ -31,24 +32,49 @@ def home():
     return render_template('home.html', recent=expand_recent_bins())
 
 
-@app.endpoint('views.bin')
-def bin(name):
+@app.endpoint('views.inspect_bin')
+def inspect_bin(name):
     try:
         bin = db.lookup_bin(name)
     except KeyError:
         return "Not found\n", 404
-    if request.query_string == 'inspect':
-        if bin.private and session.get(bin.name) != bin.secret_key:
-            return "Private bin\n", 403
-        update_recent_bins(name)
-        return render_template('bin.html',
-            bin=bin,
-            base_url=request.scheme+'://'+request.host)
-    else:
-        db.create_request(bin, request)
-        resp = make_response("ok\n")
-        resp.headers['Sponsored-By'] = "https://www.runscope.com"
-        return resp
+
+    if bin.private and session.get(bin.name) != bin.secret_key:
+        return "Private bin\n", 403
+    update_recent_bins(name)
+    return render_template('bin.html',
+        bin=bin,
+        base_url=request.scheme+'://'+request.host)
+
+
+@app.endpoint('views.bin')
+def bin(name):
+    parts = name.split('/', 1)
+    bin_and_host = parts[0]
+    path = parts[1] if len(parts) > 1 else ''
+
+    bin_host_parts = bin_and_host.split('-')
+    bin_name = bin_host_parts[-1]
+    host = bin_host_parts[0] if len(bin_host_parts) > 1 else ''
+
+    url = '%s://%s/%s?%s' % (request.scheme, host, path, request.query_string) if host else ''
+
+    try:
+        bin = db.lookup_bin(bin_name)
+    except KeyError:
+        return "Not found\n", 404
+
+    db.create_request(bin, request)
+    
+    proxy_req = requests.request(
+        method=request.method,
+        url=url,
+        headers={key: value for (key, value) in request.headers if (key != 'Host' and (key not in ('Content-Length', 'Content-Type') or value))},
+        data=request.get_data(),
+        cookies=request.cookies,
+        stream=True)
+
+    return Response(stream_with_context(proxy_req.iter_content()), content_type=proxy_req.headers.get('content-type'))
 
 
 @app.endpoint('views.docs')
